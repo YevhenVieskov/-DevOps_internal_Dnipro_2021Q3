b95b4457a511cc143a781355be5aa88d15d50963
YevhenVieskov <evveskov@gmail.com>
2021-09-26 21:57:34 +0300

    new files is added

--- iac/jmaster_setup.sh
+++ iac/jmaster_setup.sh
@@ -15,13 +15,20 @@ ansible-galaxy install geerlingguy.jenkins
 
 cd ~
 git clone https://github.com/YevhenVieskov/DevOps_internal_Dnipro_2021Q3.git
-cd ~/DevOps_internal_Dnipro_2021Q3
-cp -r jenkins_config     /var/lib/jenkins
 
+#install Jenkins
+cp  ~/DevOps_internal_Dnipro_2021Q3/ansible/install_jenkins.yml ~/.ansible/
+ansible-playbook ~/.ansible/install_jenkins.yml
 #install docker
 cp -r ~/DevOps_internal_Dnipro_2021Q3/ansible/install_docker ~/.ansible/roles
 cp  ~/DevOps_internal_Dnipro_2021Q3/ansible/install_docker.yml ~/.ansible/
 ansible-playbook ~/.ansible/install_docker.yml
+#install Jenkins config and jobs
+cd ~/DevOps_internal_Dnipro_2021Q3
+cp -r jenkins_config     /var/lib/jenkins
+#add jenkins to docker group
+usermod -aG docker jenkins
+reboot
 
 
      
